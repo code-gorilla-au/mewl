0e8b9e257095e2f7d15bdf5219dcf223401057a6
mauricio riquelme <m.riquelme223@gmail.com>
2022-08-20 16:10:37 +1000

    docs

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # mewl
 go utility library delivering modularity, performance &amp; extras
+
+## Install
+
+```bash
+go get -u github.com/code-gorilla-au/mewl
+```
+
+## Doc
--- types.go
+++ types.go
@@ -1,7 +1,10 @@
 package mewl
 
+// ComposeFunc - function that receives an input and returns an input of the same type.
 type ComposeFunc[T any] func(T) T
 
+// PredicateFunc - function that receives an input and returns a boolean value.
 type PredicateFunc[T any] func(item T) bool
 
+// MapperFunc - transform function that receives an input and returns a new type.
 type MapperFunc[T any, K any] func(item T) K
