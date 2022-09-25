daf264288a5def793fdeb21fc53ee2da07dbec07
mauricio riquelme <m.riquelme223@gmail.com>
2022-09-25 14:38:34 +1000

    adding map func

--- maps.go
+++ maps.go
@@ -17,3 +17,26 @@ func MapValues[T comparable, K any](obj map[T]K) []K {
 	}
 	return result
 }
+
+// MapOmitKeys - returns a partial copy of an object omitting the keys specified.
+// If the key does not exist, the property is ignored.
+func MapOmitKeys[T comparable, K any](obj map[T]K, omits ...T) map[T]K {
+	for _, omit := range omits {
+		delete(obj, omit)
+	}
+	return obj
+}
+
+// MapPickKeys- Returns a partial copy of an object containing only the keys specified.
+// If the key does not exist, the property is ignored.
+func MapPickKeys[T comparable, K any](obj map[T]K, picks ...T) map[T]K {
+	result := make(map[T]K, 0)
+
+	for _, pick := range picks {
+		if value, ok := obj[pick]; ok {
+			result[pick] = value
+		}
+	}
+
+	return result
+}
--- maps_test.go
+++ maps_test.go
@@ -26,3 +26,39 @@ func TestMapValues(t *testing.T) {
 		assert.Contains(t, got, item)
 	})
 }
+
+func TestMapOmitKey(t *testing.T) {
+	got := MapOmitKeys(map[string]int{"1": 1, "2": 2}, "2")
+
+	assert.Equal(t, map[string]int{"1": 1}, got)
+}
+
+func TestMapOmitKey_missing_key_returns_whole_object(t *testing.T) {
+	got := MapOmitKeys(map[string]int{"1": 1, "2": 2}, "9")
+
+	assert.Equal(t, map[string]int{"1": 1, "2": 2}, got)
+}
+
+func TestMapPickKey(t *testing.T) {
+	want := map[int]int{1: 1, 2: 2}
+
+	got := MapPickKeys(map[int]int{1: 1, 2: 2, 3: 3}, 1, 2)
+
+	assert.Equal(t, want, got)
+}
+
+func TestMapPickKey_missing_keys(t *testing.T) {
+	want := map[int]int{2: 2}
+
+	got := MapPickKeys(map[int]int{1: 1, 2: 2, 3: 3}, 99, 2)
+
+	assert.Equal(t, want, got)
+}
+
+func TestMapPickKey_no_keys(t *testing.T) {
+	want := map[int]int{}
+
+	got := MapPickKeys(map[int]int{1: 1, 2: 2, 3: 3})
+
+	assert.Equal(t, want, got)
+}
