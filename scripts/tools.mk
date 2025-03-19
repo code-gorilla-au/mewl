4b1a1d70865ddb2f56aead7b235e3679b6731437
muz-ooh <mauricio.riquelme@oohmedia.com.au>
2025-03-20 06:52:51 +1100

    adding tools

--- scripts/tools.mk
+++ scripts/tools.mk
@@ -4,3 +4,4 @@ get-tools: ## Get tools used
 	go install github.com/securego/gosec/v2/cmd/gosec@latest
 	go install github.com/golangci/golangci-lint/cmd/golangci-lint@latest
 	go install github.com/princjef/gomarkdoc/cmd/gomarkdoc@latest
+	go install github.com/mitranim/gow@latest
